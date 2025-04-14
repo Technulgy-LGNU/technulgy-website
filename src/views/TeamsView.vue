aa20ee804726ad2ab0243a3b91827ce5a239f172
Elias Braun <braunelias@tghd.email>
2025-04-14 15:19:45 +0200

    Added one more team and event, updated README.md

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -3,7 +3,7 @@ import { useI18n} from 'vue-i18n'
 import { computed } from 'vue'
 const { t } = useI18n()
 
-const teams = computed(() => [
+const teamsActive = computed(() => [
   {
     name: 'Team Faabs',
     description: t('teams.teamfaabs'),
@@ -55,16 +55,57 @@ const teams = computed(() => [
     ]
   }
 ])
+
+const teamsInactive = computed(() => [
+  {
+    name: 'Team Robotronic',
+    description: t('teams.robotronic'),
+    image: '/images/teams/team-robotronic.JPG',
+    awards: [
+      t('teams.robotronic_go2024'),
+      t('teams.robotronic_vo2024'),
+      t('teams.robotronic_wo2023-st'),
+      t('teams.robotronic_go2023'),
+      t('teams.robotronic_go2023-st'),
+      t('teams.robotronic_vo2023'),
+      t('teams.robotronic_vo2019'),
+    ]
+  }
+])
 </script>
 
 <template>
   <section class="bg-white py-12 px-4 md:px-16 text-gray-800">
+    <h1 class="text-3xl md:text-4xl font-bold text-center mb-12">{{ t('teams.title') }}</h1>
     <div class="max-w-7xl mx-auto">
-      <h1 class="text-3xl md:text-4xl font-bold text-center mb-12">{{ t('teams.title') }}</h1>
+      <h1 class="text-3xl md:text-4xl font-bold text-center mb-12">{{ t('teams.active') }}</h1>
 
       <div class="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
         <div
-          v-for="team in teams"
+          v-for="team in teamsActive"
+          :key="team.name"
+          class="bg-gray-50 rounded-2xl shadow-md hover:shadow-lg transition p-6 flex flex-col"
+        >
+          <img :src="team.image" :alt="team.name" class="w-full h-48 object-cover rounded-xl mb-4" />
+
+          <h2 class="text-xl font-semibold mb-2">{{ team.name }}</h2>
+          <p class="text-sm text-gray-600 mb-4">{{ team.description }}</p>
+
+          <div>
+            <h3 class="font-medium mb-1 text-gray-700">{{ t('teams.awards') }}:</h3>
+            <ul class="list-disc list-inside text-sm text-gray-700">
+              <li v-for="award in team.awards" :key="award">{{ award }}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="max-w-7xl mx-auto">
+      <h1 class="text-3xl md:text-4xl font-bold text-center mb-12">{{ t('teams.inactive') }}</h1>
+
+      <div class="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+        <div
+          v-for="team in teamsInactive"
           :key="team.name"
           class="bg-gray-50 rounded-2xl shadow-md hover:shadow-lg transition p-6 flex flex-col"
         >
