cb6848f6345c64465f5135bfa0765758b9f4773b
Elias Braun <braunelias@tghd.email>
2026-05-06 17:44:06 +0200

    added sponsor ingenics consulting

--- src/views/SponsorsView.vue
+++ src/views/SponsorsView.vue
@@ -51,6 +51,11 @@ const sponsors = [
     url: 'https://www.maxongroup.com/de-de',
   },
   {
+    name: 'Ingenics Consulting',
+    logo: '/images/sponsors/ingenics_consulting.webp',
+    url: 'https://www.ingenics.com/de/',
+  },
+  {
     name: 'Volksbank-Stiftung Ulm-Biberach',
     logo: '/images/sponsors/volksbankstiftungulmbiberach.webp',
     url: 'https://www.volksbank-ulm-biberach.de/startseite.html',
